936f7feef7c883e65eb222c2ef23eb421e06d27d
cypo <80849671+yungcypo@users.noreply.github.com>
2022-03-24 22:57:31 +0100

    Add files via upload

---
+++ Hangman/srandicky.py
@@ -0,0 +1,24 @@
+import random
+'''
+# txt file
+file = open("assets\words\colors.txt", "r")
+f = file.readlines()
+
+wordslist = []
+
+for line in f:
+    wordslist.append(line.strip())
+
+print(wordslist)
+file.close()
+'''
+
+
+'''
+list = ['red', 'orange', 'yellow', 'green', 'blue', 'cyan', 'purple', 'pink', 'white', 'gray', 'black']
+word = random.choice(list)
+print(word)
+print(len(word))
+'''
+
+
